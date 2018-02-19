090c4f08a7de01d42e14b91cd87aeb12418c029e
RinRin15 <36575967+RinRin15@users.noreply.github.com>
2018-02-19 16:17:10 +0300

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1519045945,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/фонобрезанный-sheet0.png",
+		"images/Плита-sheet0.png",
+		"images/Уменьшение_огня-sheet0.png",
+		"images/Усиление_огня-sheet0.png",
+		"images/Стрелка-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/Стрелка2-sheet0.png",
+		"images/Стрелка2-sheet1.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
